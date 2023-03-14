46be14efec2f3f0fcde498226377e16c2769141c
devikakarpe <devikankarpe@gmail.com>
2023-03-14 19:16:16 +0530

    added logic for calculating length of line

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Geometry Line Program");
+        int x1 = 9;
+        int y1 = 5;
+        int x2 = 6;
+        int y2 = 8;
+        double length = Math.sqrt((x2-x1)^2 + (y2-y1)^2);
+        System.out.println("Length Of Line is:"+length);
     }
 }
